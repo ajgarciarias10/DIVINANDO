aef023d22b3cbada28a4613c9d80f15de4f9be6f
Antonio José García Arias <73525861+ajgarciarias10@users.noreply.github.com>
2022-04-05 17:26:59 +0200

    TONI FALTA HACER LA CONEXION CON FIREBAASE PERO VALIDACIONES HECHAS

---
+++ BACKENDTONI/app/src/main/java/com/ieszv/deintyad/backendtoni/Functions.kt
@@ -0,0 +1,15 @@
+package com.ieszv.deintyad.backendtoni
+
+import android.content.Context
+import android.widget.Toast
+import com.chaquo.python.PyObject
+import com.chaquo.python.Python
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlin.concurrent.timerTask
+
+class Functions {
+
+
+}
--- BACKENDTONI/app/src/main/java/com/ieszv/deintyad/backendtoni/RegisterActivity.kt
+++ BACKENDTONI/app/src/main/java/com/ieszv/deintyad/backendtoni/RegisterActivity.kt
@@ -1,8 +1,11 @@
 package com.ieszv.deintyad.backendtoni
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import com.chaquo.python.PyObject
 import com.chaquo.python.Python
 import com.chaquo.python.android.AndroidPlatform
@@ -12,15 +15,13 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
+    //lateinit var auth :FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
+       // auth = Firebase.auth
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-        if (! Python.isStarted()) {
-            Python.start(AndroidPlatform(this))
-        }
-        val  py = Python.getInstance()
-        val pyobj = py.getModule("FunctionsforBack")
-        initialize(pyobj)
+
+        initialize()
     }
     /**
      * ·······
@@ -28,41 +29,75 @@ class RegisterActivity : AppCompatActivity() {
      * ·······
      *
      */
-    private fun initialize(pyobj: PyObject) {
+
+    private fun initialize() {
+
+        //region Como obtener las funciones de Python
+            if (! Python.isStarted()) {
+                Python.start(AndroidPlatform(this))
+            }
+            val py: Python = Python.getInstance()
+            val pyObj: PyObject = py.getModule("ValidatingEmailAndPassword")
+        //endregion
         //region CAMPOS DE TIPO INPUT EDIT TEXT
-        lateinit var  rEmail: TextInputEditText
-        rEmail = findViewById(R.id.inputEmail)
-        lateinit var  rPassword : TextInputEditText
-        rPassword = findViewById(R.id.inputContraseña)
-        lateinit var  rRPasword : TextInputEditText
-        rRPasword = findViewById(R.id.inputRPContraseña)
+        val rEmail: TextInputEditText = findViewById(R.id.inputEmail)
+        val rPassword : TextInputEditText = findViewById(R.id.inputContraseña)
+        val rRPasword : TextInputEditText = findViewById(R.id.inputRPContraseña)
         //endregion
         //region  CAMPOS DE TIPO BUTTON
-        var  registerButton : Button = findViewById(R.id.bt_registrar)
+        val registerButton : Button = findViewById(R.id.bt_registrar)
         //region REGISTER ONCLICK EVENTO
             registerButton.setOnClickListener {
                 val  email = rEmail.text.toString()
-                var obj = pyobj.callAttr("isEMAILValid",email)
+                val obj = pyObj.callAttr("isEMAILValid",email)
+
+
                 val  password = rPassword.text.toString()
-                var obj2 = pyobj.callAttr("isEMAILValid",email)
+                val ob2 = pyObj.callAttr("isPasswordValid",password)
                 val  repeatPassword = rRPasword.text.toString()
-                //FUNCION QUE CHECKEA LOS CAMPOS INPUTEDIT EN PYTHON
+
+                // region FUNCION QUE CHECKEA LOS CAMPOS INPUTEDIT EN PYTHON
+                    if(obj.toBoolean() && ob2.toBoolean()) {
+                        rRPasword.visibility = View.VISIBLE
+                        if(repeatPassword == password){
+                            Toast.makeText(applicationContext, "BIENVENIDO$email", Toast.LENGTH_LONG).show()
+                            //registerUser(email,password)
+                        }
+                        else{
+                            rRPasword.error = "Error no es similiar a la contraseña de arriba"
+                        }
+                    }
+
+                    else if(!obj.toBoolean()){
+                        rEmail.error = "Error introduzca el email correctamente"
+                    }
+                     if (!ob2.toBoolean()){
+                         rPassword.error = "La contraseña elegida no es segura: debe contener letras minúsculas, mayusculas, números alfanumericos y  al menos 1 carácter no alfanumérico @/&etc..."
+                    }
+
+                //endregion
+
 
 
             }
-        //endregion
-        var tengoCuenta : Button = findViewById(R.id.bt_tencuenta)
-        tengoCuenta.setOnClickListener {
-
-        }
-
 
         //endregion
-        //region FIREBASEAUTH
-            lateinit var auth : FirebaseAuth
-            auth = Firebase.auth
-        //endregion
-
-
     }
+    //FALTA  LOGIN FIREBASE
+  /*  fun registerUser(email: String ,password : String){
+
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this) {
+                task ->
+            if (task.isSuccessful){
+                startActivity(Intent(this,LoginActivity::class.java))
+                finish()
+            }
+            else{
+                Toast.makeText(applicationContext,"Register Failed",Toast.LENGTH_LONG).show()
+
+            }
+        }
+    }
+
+   */
 }
