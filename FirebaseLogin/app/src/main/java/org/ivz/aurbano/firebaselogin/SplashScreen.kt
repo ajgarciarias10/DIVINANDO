197a254af21724157a86791fdc44d1d49549bf17
Alvaro <alvarourmu@gmail.com>
2022-04-17 19:05:27 +0200

    Urbano - Actualizacion Splash Screen e Interfaz Juego

---
+++ FirebaseLogin/app/src/main/java/org/ivz/aurbano/firebaselogin/SplashScreen.kt
@@ -0,0 +1,11 @@
+package org.ivz.aurbano.firebaselogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
