dc6f857c872a1f55ac5caa6808646d3e92a13da2
Alvaro <alvarourmu@gmail.com>
2022-05-04 21:20:44 +0200

    Urbano - Proyecto con fragmentos

---
+++ AdivinandoFrontEnd/app/src/main/java/org/ivz/aurbano/adivinandofrontend/Game.kt
@@ -0,0 +1,11 @@
+package org.ivz.aurbano.adivinandofrontend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Game : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_game)
+    }
+}
