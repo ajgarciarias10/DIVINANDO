7cbe43fb7d9968d11e05dfc4f62644d4cd205941
Antonio José García Arias <73525861+ajgarciarias10@users.noreply.github.com>
2022-05-06 16:09:43 +0200

    Me falta mostrar la imagen

--- PROYECTOWORDLE/BackENDWITHFRAGMENTS/app/src/main/java/com/ieszv/progmulti/backendwithfragments/CountryFragment.kt
+++ PROYECTOWORDLE/BackENDWITHFRAGMENTS/app/src/main/java/com/ieszv/progmulti/backendwithfragments/CountryFragment.kt
@@ -1,30 +1,46 @@
 package com.ieszv.progmulti.backendwithfragments
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.LinearLayout
+import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.RequestOptions
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.storage.StorageReference
 import com.ieszv.progmulti.backendwithfragments.databinding.FragmentCountryBinding
+
 
 class CountryFragment : Fragment() {
 
 
-    lateinit var sr: StorageReference
-    private  lateinit var  container:LinearLayout
-    var arraylistPaises = ArrayList<String>()
-     var pais = String()
-    //Variable para hacer el findByViewID
-    private var _binding: FragmentCountryBinding? = null
-    private val binding get() = _binding!!
+    //Contenedor que inicializamos por que luego mas tarde lo vamos a inflar para meterle una vista
+    private lateinit var container: LinearLayout
 
-    private  val db = FirebaseFirestore.getInstance()
+    //region Lista de Paises en el idioma español,ingles y sus respectivas urls
+        var listaDePaisesEnEspañol = ArrayList<String>()
+        var listaDePaisesEnIngles = ArrayList<String>()
+        var urls = ArrayList<String>()
+    //endregion
+    //region Variable para hacer el findByViewID
+        private var _binding: FragmentCountryBinding? = null
+        private val binding get() = _binding!!
+    //endregion
+    //region Cogiendo la instancia de la bd de Firestore
+        private val db = FirebaseFirestore.getInstance()
+    //endregion
 
-
+    /**
+     * Metodo  en el momento de crear la vista
+     */
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -34,44 +50,59 @@ class CountryFragment : Fragment() {
 
 
     }
+    /**
+     * Metodo al crearse la vista
+     */
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         despliegaEditText()
     }
 
+
     fun despliegaEditText() {
         container = binding.linear
-
         db.collection("Paises").document("ListaPaises").get().addOnSuccessListener { document ->
-         arraylistPaises.add(document.data?.get("Paises").toString())
-        }
-        for (i in 0 until arraylistPaises.size) {
-            // generating the index using Math.random()
-            val index = (Math.random() * arraylistPaises.size) as Int
 
-            pais = arraylistPaises.get(index)
-
-            Log.v(
-                "Random Element is :",  arraylistPaises.get(index)
-            )
-        }
-        for (i in 0 until pais.length) {
-            val duplicateThis = LayoutInflater.from(requireContext())
-                .inflate(R.layout.duplicate_this, null) as LinearLayout
-            container.addView(duplicateThis)
+            /**
+             * He convertido
+             */
+            listaDePaisesEnEspañol = document.data?.get("Paises") as ArrayList<String>
+            listaDePaisesEnEspañol.forEach { entry ->
+                //Log.v("Paises",entry)
+            }
 
 
-        }
+            listaDePaisesEnIngles = document.data?.get("EnglishCountries") as ArrayList<String>
+            listaDePaisesEnIngles.forEach { entry ->
 
+                //Log.v("Paises Ingleses",entry)
+            }
+            urls = document.data?.get("Url") as ArrayList<String>
 
-
-
-
-    }
+            val index = (Math.random() * listaDePaisesEnIngles.size).toInt()
+            var pais = listaDePaisesEnIngles.get(index)
+            Log.v("Paises ",pais)
+            listaDePaisesEnIngles.get(index)
+            Log.v("Paises ",index.toString())
 
 
 
+             for (i in 0 until pais.length) {
+                 val duplicateThis = LayoutInflater.from(requireContext())
+                     .inflate(R.layout.duplicate_this, null) as FrameLayout
+                 container.addView(duplicateThis)
 
 
+
+            }
+
+
+
+}
+
+
+
+
+}
 }
