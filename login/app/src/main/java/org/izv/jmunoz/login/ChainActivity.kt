2f10533bad77ad0a718a9f195dd5ed92a387f85c
Milito87 <juanmamilito23@gmail.com>
2022-05-12 18:22:03 +0200

    Osorio minigames v1

--- login/app/src/main/java/org/izv/jmunoz/login/ChainActivity.kt
+++ login/app/src/main/java/org/izv/jmunoz/login/ChainActivity.kt
@@ -4,41 +4,59 @@ import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
+import androidx.constraintlayout.widget.Group
+import com.chaquo.python.PyObject
+import com.chaquo.python.Python
+import com.chaquo.python.android.AndroidPlatform
 import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlin.concurrent.thread
 import kotlin.random.Random
 
 class ChainActivity : AppCompatActivity() {
 
-    private val db = FirebaseFirestore.getInstance()
     lateinit var chainA1: TextView
     lateinit var chainA2: TextView
     lateinit var chainA3: TextView
     lateinit var chainA4: TextView
     lateinit var chainA5: TextView
+
     lateinit var chainB1: TextView
     lateinit var chainB2: TextView
     lateinit var chainB3: TextView
     lateinit var chainB4: TextView
     lateinit var chainB5: TextView
+
+    lateinit var chainC1: TextView
+    lateinit var chainC2: TextView
+    lateinit var chainC3: TextView
+    lateinit var chainC4: TextView
+    lateinit var chainC5: TextView
+
+    lateinit var groupA: Group
+    lateinit var groupB: Group
+    lateinit var groupC: Group
+
     lateinit var score: TextView
-    lateinit var etChain: EditText
     lateinit var btNext: Button
-    lateinit var  word: String
+    //word
+    lateinit var word: String
     lateinit var DBword: String
-    lateinit var  answer: String
-    lateinit var aux: String
-    lateinit var text: String
+    lateinit var answerText: String
+
+    //Python
+    lateinit var py: Python
+    lateinit var pyObj: PyObject
+    //data
+    private val db = FirebaseFirestore.getInstance()
+    lateinit var listado:  MutableList<DocumentSnapshot>
+    var idWord = -1
+    //Game var
     var round = 1
     var points = 0
-    var size = 834
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,62 +66,10 @@ class ChainActivity : AppCompatActivity() {
         init()
 
         btNext.setOnClickListener {
-            if(round < 5){
-                comprobation(it)
-            }
-            else{
-                startActivity(Intent(this,EndActivity::class.java))
-            }
+            isEnd(it)
         }
 
     }
-
-    private fun init(){
-
-        chainA1 = findViewById(R.id.tvChainA1)
-        chainA2 = findViewById(R.id.tvChainA2)
-        chainA3 = findViewById(R.id.tvChainA3)
-        chainA4 = findViewById(R.id.tvChainA4)
-        chainA5 = findViewById(R.id.tvChainA5)
-        chainB1 = findViewById(R.id.tvChainB1)
-        chainB2 = findViewById(R.id.tvChainB2)
-        chainB3 = findViewById(R.id.tvChainB3)
-        chainB4 = findViewById(R.id.tvChainB4)
-        chainB5 = findViewById(R.id.tvChainB5)
-        score = findViewById(R.id.tvScore)
-        btNext = findViewById(R.id.btNextChain)
-        etChain = findViewById(R.id.etWordChain)
-        score.text = "Round "+round+"    "+points+" Points"
-        word = ""
-        //LEER DATOS
-        var id = Random.nextInt(0, size - 1)
-        db.collection("Palabras").document(id.toString()).get()
-            .addOnSuccessListener{
-                word = it.get("nombre").toString()
-                answer(0)
-            }
-    }
-
-    private fun comprobation(view: View){
-        if(etChain.text.length == 5){
-            DBword = "null"
-            text = etChain.text.toString().uppercase()
-                for (i in 0..size) {
-                    db.collection("Palabras").document(i.toString()).get().addOnSuccessListener {
-                        aux = it.get("nombre").toString()
-                        if (text == aux) {
-                            DBword = text
-
-                        }
-                    }
-                }
-            found()
-        }
-        else{
-            Snackbar.make(view, "WORD LENGTH 5", Snackbar.LENGTH_LONG).show()
-        }
-    }
-
     private fun answer(i: Int){
         if(i == 0){
             chainA1.text = word[0].toString()
@@ -129,30 +95,141 @@ class ChainActivity : AppCompatActivity() {
             chainA2.text = DBword[4].toString()
         }
         else if(i == 2){
-            chainB1.text = etChain.text[0].toString()
-            chainB2.text = etChain.text[1].toString()
-            chainB3.text = etChain.text[2].toString()
-            chainB4.text = etChain.text[3].toString()
-            chainB5.text = etChain.text[4].toString()
+            chainB1.text = chainC1.text
+            chainB2.text = chainC2.text
+            chainB3.text = chainC3.text
+            chainB4.text = chainC4.text
+            chainB5.text = chainC5.text
             chainB1.setBackgroundColor(Color.MAGENTA)
             chainB2.setBackgroundColor(Color.MAGENTA)
             chainB3.setBackgroundColor(Color.RED)
             chainB4.setBackgroundColor(Color.RED)
             chainB5.setBackgroundColor(Color.RED)
         }
-        etChain.setText("")
+        chainC1.text = ""
+        chainC2.text = ""
+        chainC3.text = ""
+        chainC4.text = ""
+        chainC5.text = ""
     }
-
-    private fun found(){
-        if (DBword != "null") {
+    private fun buildWord(){
+        answerText = ""+chainC1.text+chainC2.text+chainC3.text+chainC4.text+chainC5.text
+        answerText = answerText.uppercase()
+    }
+    private fun comprobation(view: View){
+        if(isEmptyChar()) {
+            Snackbar.make(view, "Error  5 length word please.", Snackbar.LENGTH_LONG).setAction("hidden") {}.show()
+        }
+        else{
+            nextRound()
+        }
+    }
+    private fun foundWord(){
+        DBword = ""
+        var i = 0
+        //existe la palabra en DB?
+        while (i < listado.size) {
+            if (listado[i].get("nombre") == answerText) {
+                DBword = answerText // si existe
+                i = listado.size //acabar bucle
+            }
+            i++
+        }
+        if (DBword != "") {
             answer(1)
             points++
         } else {
             answer(2)
             points--
         }
-        round++
-        score.text = "Round " + round + "    " + points + " Points"
     }
+    private fun getPythonFile(name: String): PyObject {
+        if (! Python.isStarted()) {
+            Python.start( AndroidPlatform(this));
+        }
+        py = Python.getInstance()
+        var pytobj = py.getModule(name) //give python script name
+        return pytobj
+    }
+    private fun init(){
+        instanciation()
+        //Load data
+        db.collection("Palabras").get().addOnSuccessListener {
+            listado = it.documents
+            idWord = Random.nextInt(0, listado.size - 1)
+            word = listado[idWord].get("nombre").toString()
+            answer(0)
+        }
 
+        loadScreen()
+
+
+    }
+    private fun instanciation(){
+        chainA1 = findViewById(R.id.tvChainA1)
+        chainA2 = findViewById(R.id.tvChainA2)
+        chainA3 = findViewById(R.id.tvChainA3)
+        chainA4 = findViewById(R.id.tvChainA4)
+        chainA5 = findViewById(R.id.tvChainA5)
+
+        chainB1 = findViewById(R.id.tvChainB1)
+        chainB2 = findViewById(R.id.tvChainB2)
+        chainB3 = findViewById(R.id.tvChainB3)
+        chainB4 = findViewById(R.id.tvChainB4)
+        chainB5 = findViewById(R.id.tvChainB5)
+
+        chainC1 = findViewById(R.id.tvChainC1)
+        chainC2 = findViewById(R.id.tvChainC2)
+        chainC3 = findViewById(R.id.tvChainC3)
+        chainC4 = findViewById(R.id.tvChainC4)
+        chainC5 = findViewById(R.id.tvChainC5)
+
+        groupA = findViewById(R.id.GroupA)
+        groupB = findViewById(R.id.GroupB)
+        groupC = findViewById(R.id.GroupC)
+
+        score = findViewById(R.id.tvScore)
+        btNext = findViewById(R.id.btNextChain)
+
+        listado = mutableListOf()
+        word = ""
+    }
+    private fun isEmptyChar(): Boolean{
+        if(chainC1.length() != 1 || chainC2.length() != 1 || chainC3.length() != 1 ||
+            chainC4.length() != 1 || chainC5.length() != 1) {
+            return true
+        }
+        return false
+    }
+    private fun isEnd(view: View){
+        if(round < 5){
+            comprobation(view)
+        }
+        else{
+            startActivity(Intent(this,EndActivity::class.java))
+        }
+    }
+    private fun loadScreen(){
+        //before load data
+        groupA.visibility = View.INVISIBLE
+        groupB.visibility = View.INVISIBLE
+        groupC.visibility = View.INVISIBLE
+        score.text = "Looking for characters"
+
+        Thread.sleep(5000)
+
+        //after load data
+        pyObj = getPythonFile("setText")
+        score.text = pyObj.callAttr("main",points, round).toString()
+        groupA.visibility = View.VISIBLE
+        groupB.visibility = View.VISIBLE
+        groupC.visibility = View.VISIBLE
+    }
+    private fun nextRound(){
+        buildWord()
+        foundWord()
+        round++
+        pyObj = getPythonFile("setText")
+        score.text = pyObj.callAttr("main",points, round).toString()
+    }
 }
--- login/app/src/main/java/org/izv/jmunoz/login/ClubActivity.kt
+++ login/app/src/main/java/org/izv/jmunoz/login/ClubActivity.kt
@@ -4,16 +4,21 @@ import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import com.chaquo.python.PyObject
+import com.chaquo.python.Python
+import com.chaquo.python.android.AndroidPlatform
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.squareup.picasso.Picasso
 import kotlin.random.Random
 
 class ClubActivity : AppCompatActivity() {
 
-    private val db = FirebaseFirestore.getInstance()
     lateinit var iClub: ImageView
     lateinit var tvRound: TextView
     lateinit var tvClubName: TextView
@@ -22,10 +27,18 @@ class ClubActivity : AppCompatActivity() {
     lateinit var btFalse: Button
     lateinit var imageName: String
     lateinit var name: String
-    var size = 8
+    lateinit var answer: String
+    //data
+    private val db = FirebaseFirestore.getInstance()
+    lateinit var listado:  MutableList<DocumentSnapshot>
+    lateinit var usados:  MutableList<Int>
+    //Python
+    lateinit var py: Python
+    lateinit var pyObj: PyObject
+    //Game var
     var round = 1
     var points = 0
-    lateinit var  answer: String
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,43 +47,27 @@ class ClubActivity : AppCompatActivity() {
         init()
 
         btNext.setOnClickListener {
-            if(round < 5){
-                comparation()
-            }
-            else{
-                startActivity(Intent(this,EndActivity::class.java))
-            }
+            isEnd(it)
         }
 
         btTrue.setOnClickListener {
-                answer = "true"
+                answer = R.string.true_.toString()
                 answer(0)
         }
         btFalse.setOnClickListener {
-                answer = "false"
+                answer = R.string.false_.toString()
                 answer(1)
         }
 
     }
-
-    private fun comparation(){
-        if(imageName == name && answer == "true"){
-            points++
-        }
-        else if(imageName != name && answer == "false"){
-            points++
-        }
-        round++
-        tvRound.text = "ROUND "+round+"     "+points+" Points"
-        answer(2)
-        getClub()
-    }
-
     private fun answer(i: Int){
+        //reset values
         btTrue.setBackgroundColor(Color.TRANSPARENT)
         btTrue.setTextColor(Color.GREEN)
         btFalse.setBackgroundColor(Color.TRANSPARENT)
         btFalse.setTextColor(Color.RED)
+
+        //set values
         if(i == 0){
             btTrue.setBackgroundColor(Color.GREEN)
             btTrue.setTextColor(Color.WHITE)
@@ -81,46 +78,124 @@ class ClubActivity : AppCompatActivity() {
         }
     }
 
+    private fun comparation(){
+        if(imageName == name && answer == R.string.true_.toString()){
+            points++
+        }
+        else if(imageName != name && answer == R.string.false_.toString()){
+            points++
+        }
+    }
+
+    private fun foundIdDB(id: Int): Boolean{
+        var i = 0
+        while(i < usados.size){
+            if(id == usados[i]){
+                return true
+            }
+            i++
+        }
+        return false
+    }
+
+    private fun getClub(){
+        var num = Random.nextInt(0, 100)
+        var id1 = RandomNum()
+        var id2 = RandomNum()
+
+        //Load True data
+        if(num < 50 ) { //true
+            Picasso.get().load(listado[id1].get("url").toString()).into(iClub)
+            imageName = listado[id1].get("nombre").toString()
+            name = imageName
+            tvClubName.text = imageName
+            usados.add(id1)
+        }
+        //Load False data
+        else{
+            Picasso.get().load(listado[id1].get("url").toString()).into(iClub)
+            imageName = listado[id1].get("nombre").toString()
+            name = listado[id2].get("nombre").toString()
+            tvClubName.text = name
+            usados.add(id1)
+        }
+    }
+
+    private fun getPythonFile(name: String): PyObject{
+        if (! Python.isStarted()) {
+            Python.start( AndroidPlatform(this));
+
+        }
+        py = Python.getInstance()
+        var pytobj = py.getModule(name) //give python script name
+        return pytobj
+    }
+
     private fun init(){
+        //to assign
         iClub = findViewById(R.id.iClub)
         btTrue = findViewById(R.id.btTrue)
         btFalse = findViewById(R.id.btFalse)
         tvRound = findViewById(R.id.tvRound)
         tvClubName = findViewById(R.id.tvClubName)
         btNext = findViewById(R.id.btNextClub)
-        tvRound.text = "ROUND "+round+"     "+points+" Points"
+        listado = mutableListOf()
+        usados = mutableListOf()
+
+        //set values
+        tvRound.text = "ROUND $round     $points Points"
+        answer = ""
         btTrue.setBackgroundColor(Color.TRANSPARENT)
         btFalse.setBackgroundColor(Color.TRANSPARENT)
+
+        //get club list
+        db.collection("Clubs").get().addOnSuccessListener {
+            listado = it.documents
+            getClub()
+        }
+    }
+
+    private fun isEnd(view: View){
+
+        if(round < 5){
+            if(answer == ""){
+                Snackbar.make(view, "Select your answer before to next", Snackbar.LENGTH_LONG).setAction("hidden") {}.show()
+            }
+            else{
+                comparation()
+                nextRound()
+            }
+
+        }
+        else{
+            startActivity(Intent(this,EndActivity::class.java))
+        }
+    }
+
+    private fun nextRound(){
+        round++
+        answer = ""
+        pyObj = getPythonFile("setText")
+        tvRound.text = pyObj.callAttr("main",points, round).toString()
+        answer(2)
         getClub()
     }
 
-    private fun getClub(){
-        var clubID = Random.nextInt(0, size - 1)
-        var clubID2 = Random.nextInt(0, size - 1)
-        var num = Random.nextInt(0, 100)
+    private fun RandomNum(): Int{
+        var numId = -1
+        var found = 0
 
-        //LEER DATOS
-        if(num < 50 ) { //true
-            db.collection("Clubs").document(clubID.toString()).get()
-                .addOnSuccessListener {
-                    imageName = it.get("nombre").toString()
-                    tvClubName.text = imageName
-                    name = imageName
-                    Picasso.get().load(it.get("url").toString()).into(iClub)
-                }
+        pyObj = getPythonFile("validateNum") //python file name == "validateNum"
+        numId = pyObj.callAttr("main",(listado.size- 1)).toInt() //python def name == "main"
+
+        //mientras coincida numId con algun Id de la base datos
+        while(found == 0) {
+            numId = pyObj.callAttr("main", (listado.size - 1)).toInt()
+            if (!foundIdDB(numId)) {
+                found = 1
+            }
         }
-        else{ //false
-            db.collection("Clubs").document(clubID.toString()).get()
-                .addOnSuccessListener {
-                    imageName = it.get("nombre").toString()
-                    Picasso.get().load(it.get("url").toString()).into(iClub)
-                }
-            db.collection("Clubs").document(clubID2.toString()).get()
-                .addOnSuccessListener {
-                    name = it.get("nombre").toString()
-                    tvClubName.text = name
-                }
-        }
+        return numId
     }
 
 }
