3c25b621d4f81dc82eaf0f18541a92bdd74532fc
Antonio José García Arias <73525861+ajgarciarias10@users.noreply.github.com>
2022-05-12 22:40:28 +0200

    CACA

--- Adivinando/Adivinando/app/src/main/java/com/divinando/tfg/adivinando/ui/GameModes/modNormal/NormalMode.kt
+++ Adivinando/Adivinando/app/src/main/java/com/divinando/tfg/adivinando/ui/GameModes/modNormal/NormalMode.kt
@@ -1,10 +1,12 @@
 package com.divinando.tfg.adivinando.ui.GameModes.modNormal
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import com.divinando.tfg.adivinando.databinding.FragmentNormalModeBinding
 import com.divinando.tfg.adivinando.model.entity.GameObjeto
 
@@ -18,6 +20,7 @@ class NormalMode : Fragment() {
     private val binding get() = _binding!!
     lateinit var objeto: GameObjeto
     var bundle = Bundle()
+    lateinit var palabraDiccionario : String
 
 
     override fun onCreateView(
@@ -35,17 +38,63 @@ class NormalMode : Fragment() {
         initialize()
     }
 
-    fun initialize(){
-         bundle = requireArguments()
-         objeto = bundle.getSerializable("juegos") as GameObjeto
+    fun initialize() {
+        //Sacamos los argumentos de los objetos
+        bundle = requireArguments()
+        //Sacamos el objeto que nos pasa en este caso es del modo de juego  normal
+        objeto = bundle.getSerializable("juegos") as GameObjeto
+        //region cogemos una palabra del diccionario aleatoria
+            do {
+                //region indice aleatorio para coger un dato aleatorio
+                    val index = (Math.random() * objeto.listaPalabras!!.size).toInt()
+                //endregion
+                 palabraDiccionario = objeto.listaPalabras!![index]
 
+             } while (palabraDiccionario.length == 6 && palabraDiccionario.length == 7)
+        //endregion
 
+        Log.v("Palabra",palabraDiccionario)
+
+        if (palabraDiccionario.length == 6){
+
+            binding.group5palabras.visibility = View.VISIBLE
+            cinLetr(palabraDiccionario)
+
+        }else {
+            binding.group6pf1.visibility = View.VISIBLE
+            seisletr(palabraDiccionario)
+        }
+
+    }
+    fun cinLetr(palabraDiccionario:String){
+        binding.btNext.setOnClickListener {
+            var palabrafila1 = binding.f1p1.text.toString() +
+                               binding.f1p2.text.toString() +
+                               binding.f1p3.text.toString() +
+                               binding.f1p4.text.toString() +
+                               binding.f1p5.text.toString()
+            if(palabrafila1 == palabraDiccionario){
+                //Coincide la palabra
+            }
+            else{
+                //Comprobar letras que coinciden o si no coincidenç
+                    //Caso coincidencia en la primera posicion
+                if(binding.f1p1.text.toString() == palabraDiccionario.get(1).toString()){
+
+               // }else if(){
+
+                //}else if(){
+
+                }
+            }
+        }
 
 
     }
+    fun seisletr(palabraDiccionario:String){
 
 
-
+    }
 
 
 }
