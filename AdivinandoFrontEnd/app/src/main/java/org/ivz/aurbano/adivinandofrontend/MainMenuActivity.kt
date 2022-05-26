77baac35198e0ab913f7058bdd09d23e332036d9
Alvaro <alvarourmu@gmail.com>
2022-05-26 21:36:36 +0200

    Actualizacion AlertDialog

--- AdivinandoFrontEnd/app/src/main/java/org/ivz/aurbano/adivinandofrontend/MainMenuActivity.kt
+++ AdivinandoFrontEnd/app/src/main/java/org/ivz/aurbano/adivinandofrontend/MainMenuActivity.kt
@@ -26,10 +26,6 @@ class MainMenuActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMainMenu.toolbar)
 
-        binding.appBarMainMenu.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main_menu)
@@ -37,7 +33,7 @@ class MainMenuActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
